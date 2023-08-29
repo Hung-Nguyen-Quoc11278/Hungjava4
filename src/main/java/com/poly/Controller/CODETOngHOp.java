e73e4da643e488ca08bfa81d636e6516332db374
Hung-Nguyen-Quoc11278 <hunggoodboy34@gmail.com>
2023-08-29 15:33:05 +0700

    hi

---
+++ src/main/java/com/poly/Controller/CODETOngHOp.java
@@ -0,0 +1,18 @@
+package com.poly.Controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.poly.Constant.SessionAtt;
+import com.poly.Entity.History;
+import com.poly.Entity.User;
+import com.poly.Entity.Video;
+
+public class CODETOngHOp {
+	
+	
+	
+	
+	
+	
+}
---
+++ src/main/webapp/Common/fav.jsp
@@ -0,0 +1,2 @@
+<%-- <link rel="icon" type="image/x-icon" href="<c:url value='/Template/User/img/pngegg.png'/>"> --%>
+<link rel="icon" type="image/x-icon" href="<c:url value='/Template/User/img/pngegg.png'/>">
